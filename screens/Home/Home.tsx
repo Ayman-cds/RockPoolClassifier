ab2740f3c61515e4555317829b81cf1f60953ed4
Ayman-cds <business.aymanadil@gmail.com>
2022-10-03 14:39:38 +0800

    init

---
+++ screens/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+            <TouchableOpacity>Send Image</TouchableOpacity>
+        </View>
+    );
+};
+
+export default Home;
