ca50b5152682066a3fe45a48417f4119e2c9426b
Ayman-cds <business.aymanadil@gmail.com>
2022-10-13 13:54:16 +0800

    adds firebase config

---
+++ firebase-config.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyBgKgRmTmkrxnp2okjL-ZHW1jLK8G2E56E',
+    authDomain: 'bomba-demo.firebaseapp.com',
+    projectId: 'bomba-demo',
+    storageBucket: 'bomba-demo.appspot.com',
+    messagingSenderId: '928356886949',
+    appId: '1:928356886949:web:58a0c322fc5955aaada04c',
+    measurementId: 'G-Y3Q8TEYCPF',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
